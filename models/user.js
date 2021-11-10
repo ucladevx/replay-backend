5d42ba2701fd8e43ebc5b4c9114d2d8d31415e25
Sanya <purvi.sanya@gmail.com>
2021-11-10 11:33:50 -0800

    create user and location schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
-// add schema here.
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String, // required.
+    password: String, // required.
+    spotifyUsername: String,
+    spotifyPassword: String,
+    currentLocation: String, // locationSchema id.
+    songsPlayed: [String], // id's of Song objects.
+    topArtists: [String], // names?
+    promptAnswers: [],
+    dp: String
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
