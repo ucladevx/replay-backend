b9c1413aa5f4e3d58d3179cd1cebdf4bd9f9a080
Nathan Lin <nathanlin710@gmail.com>
2022-04-26 20:16:51 -0700

    Delete user account

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,13 @@ const router = express.Router();
 // set up middleware to ensure endpoints are only accessed by authenticated users.
 // const jwtCheck = require("../helpers/jwtCheck");
 
-const { signUp, signIn, spotify, callback, refreshToken } = require("../helpers/auth");
+const { signUp, signIn, spotify, callback, refreshToken, deleteAccount } = require("../helpers/auth");
 
 router.post("/signUp", signUp);
 router.post("/signIn", signIn);
 router.post("/spotify", spotify);
 router.get("/callback", callback);
 router.get("/refreshToken", refreshToken);
+router.post("/deleteAccount", deleteAccount);
 
 module.exports = router;
